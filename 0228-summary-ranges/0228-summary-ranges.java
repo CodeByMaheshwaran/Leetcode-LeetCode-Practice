e42019f659bb739c86affcaf0cbb06d3c7853ce8
CodeByMaheshwaran <maheshwaran653@gmail.com>
2025-01-19 00:26:57 +0530

    Time: 6 ms (26.16%), Space: 41.9 MB (32.64%) - LeetHub

--- 0228-summary-ranges/0228-summary-ranges.java
+++ 0228-summary-ranges/0228-summary-ranges.java
@@ -3,14 +3,18 @@ class Solution {
         List<String> result= new ArrayList<String>();
         int n=nums.length;
         if(n==0) return result;
-        int a=nums[0];
         for(int i=0;i<n;i++){
-            if(i==n-1 || nums[i]+1 != nums[i+1]){
-                if(nums[i]!=a)result.add(a+"->"+nums[i]);
-                else result.add(a+"");
-                if(i!=n-1)a=nums[i+1];
+            int j=i;
+            while(j+1<n && nums[j]+1==nums[j+1]){
+                j++;
             }
+            if(j>i){
+                result.add(nums[i]+"->"+nums[j]);
+            }else{
+                result.add(nums[i]+"");
+            }
+            i=j;
         }
-    return result;    
+        return result;    
     }
 }
