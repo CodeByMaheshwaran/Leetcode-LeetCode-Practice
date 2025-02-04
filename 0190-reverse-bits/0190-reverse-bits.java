62823419fa9b3122327b3f4d4901264806815af9
CodeByMaheshwaran <maheshwaran653@gmail.com>
2025-02-04 17:20:42 +0530

    Time: 0 ms (100%), Space: 41 MB (99.95%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.java
+++ 0190-reverse-bits/0190-reverse-bits.java
@@ -1,14 +1,14 @@
 public class Solution {
     // you need treat n as an unsigned value
     public int reverseBits(int n) {
-        int result=0;
-        for(int i=0;i<32;i++){
-            result<<=1;
-            if( (n&1) ==1 ){
-                result+=1;
+        int result = 0;
+        for (int i = 0; i < 32; i++) {
+            result <<= 1;
+            if ((n & 1) == 1) { // n&1 --> To find the Last bit
+                result += 1;
             }
-            n>>=1;
+            n >>= 1;
         }
-    return result;    
+        return result;
     }
 }
