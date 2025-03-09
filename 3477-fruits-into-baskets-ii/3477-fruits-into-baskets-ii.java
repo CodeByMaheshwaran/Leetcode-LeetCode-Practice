f4b2f261064fceb789aa1e41dc55d8be09c785a0
CodeByMaheshwaran <maheshwaran653@gmail.com>
2025-03-10 00:02:22 +0530

    Time: 6 ms (100%), Space: 44.5 MB (100%) - LeetHub

--- 3477-fruits-into-baskets-ii/3477-fruits-into-baskets-ii.java
+++ 3477-fruits-into-baskets-ii/3477-fruits-into-baskets-ii.java
@@ -1,16 +1,17 @@
 class Solution {
     public int numOfUnplacedFruits(int[] fruits, int[] baskets) {
         int n=fruits.length;
-        int count=n;
+        Set<Integer> set=new HashSet<>();
+        int count=0;
         for(int i=0;i<n;i++){
             for(int j=0;j<n;j++){
-                if(fruits[i]<=baskets[j]){
-                    count--;
-                    baskets[j]=-1;
+                if(baskets[j]>=fruits[i] && !set.contains(j)){
+                    set.add(j);
+                    count++;
                 break;       
                 }
             }
         }
-    return count;    
+    return n-count;    
     }
 }
