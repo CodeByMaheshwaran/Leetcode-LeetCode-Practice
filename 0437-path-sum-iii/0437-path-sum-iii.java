8c6e135674e58c2dbd794c5448f3b7d63038d9b8
CodeByMaheshwaran <maheshwaran653@gmail.com>
2025-04-14 23:49:12 +0530

    Time: 19 ms (27.05%), Space: 42.9 MB (97.09%) - LeetHub

--- 0437-path-sum-iii/0437-path-sum-iii.java
+++ 0437-path-sum-iii/0437-path-sum-iii.java
@@ -14,21 +14,19 @@
  * }
  */
 class Solution {
-    int counter=0;
+    private int pathSumWithRoot(TreeNode root, long sum) {
+        if (root == null)
+            return 0;
+        int result = 0;
+        if (root.val == sum)
+            result++;
+        result += pathSumWithRoot(root.left, sum - root.val);
+        result += pathSumWithRoot(root.right, sum - root.val);
+        return result;
+    }
+
     public int pathSum(TreeNode root, int targetSum) {
         if(root==null) return 0;
-        pathSumHelper(root,targetSum,(long) 0);
-        pathSum(root.left,targetSum);
-        pathSum(root.right,targetSum);
-    return counter;    
-    }
-    private void pathSumHelper(TreeNode root, int targetSum, long currSum){
-        if(root==null) return;
-        currSum+=root.val;
-        if(currSum==targetSum){
-            counter++;
-        }
-        pathSumHelper(root.left,targetSum,(long) currSum);
-        pathSumHelper(root.right,targetSum,(long) currSum);
+    return pathSum(root.left,targetSum)+pathSumWithRoot(root,(long)targetSum)+pathSum(root.right,targetSum);
     }
 }
