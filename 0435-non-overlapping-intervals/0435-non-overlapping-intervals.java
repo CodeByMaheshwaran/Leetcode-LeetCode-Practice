bac2df4a6ee0ce2822d986dba47e1620ad7b9b97
CodeByMaheshwaran <maheshwaran653@gmail.com>
2025-04-16 23:30:15 +0530

    Time: 52 ms (25.38%), Space: 73.6 MB (60.12%) - LeetHub

---
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int eraseOverlapIntervals(int[][] intervals) {
+       if(intervals.length==0) return 0;
+       //Sort the intervals based on the end time
+       Arrays.sort(intervals, Comparator.comparingInt(a->a[1]));
+       int count=1; //we need to have atleast one interval (without overlapping)
+       int previous_interval=0; // zero based index it represents first interval
+       for(int i=1;i<intervals.length;i++){
+        //check start time of the next interval is greater than or equal to 
+        //the end time of the current interval, then we can keep it
+        if(intervals[i][0]>=intervals[previous_interval][1]){
+            previous_interval=i;
+            count++;
+        }
+       }
+    // Return the number of interval to remove (minimum)
+    return intervals.length-count;   
+    }
+}
